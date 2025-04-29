4450bd353742c5ac4346d36c97d59baa8d3817fa
alinur229035@gmail.com <alinur229035@gmail.comgit config --global user.email alinur229035@gmail.com>
2025-04-29 10:28:36 +0500

    some fixes

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
             int id = random.nextInt(10000);
-            String name = "Name" + random.nextInt(10000);
+            String name = "asdasd" + random.nextInt(10000);
             MyTestingClass testingClass = new MyTestingClass(id, name);
             Student student = new Student("Student" + i,random.nextInt(1000));
             tablica.put(testingClass, student);
         }
         for (int i = 0; i < 100; i++) {
-            System.out.println("Bucket " + i + ":"+ tablica.bucketSize(i)+" elements" );
+            System.out.println("Bucket number " + i + " have this number "+ tablica.bucketSize(i)+"of elements" );
         }
 
     }
